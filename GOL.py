d579d04d6ac78ca8fe86d91a5e15428bde61b39d
Abh1navgoel459 <123058674+Abh1navgoel459@users.noreply.github.com>
2023-04-28 13:04:32 -0700

    Update GOL.py

--- GOL.py
+++ GOL.py
@@ -22,8 +22,10 @@ import time
 
 def it_got_to_max(address, *args):
     print("Max data recieved: ", args)
-if os.path.exists(eeg_data3.csv):
-    with open(eeg_data3.csv, 'w') as f:
+
+if os.path.exists('eeg_data3.csv'):
+    with open('eeg_data3.csv', 'w') as f:
+        pass
 def write_data_to_csv(data):
     with open('eeg_data3.csv', mode='a') as csv_file:
         writer = csv.writer(csv_file)
